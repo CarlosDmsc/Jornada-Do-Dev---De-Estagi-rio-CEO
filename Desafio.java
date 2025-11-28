4a7778a72e01c6b7f5dd9e5326358cba50b76e47
mel0_dmsc <169165027+CarlosDmsc@users.noreply.github.com>
2025-11-28 10:42:40 -0300

    Update Desafio.java

--- Desafio.java
+++ Desafio.java
@@ -4,14 +4,25 @@ public class Desafio {
 
     private String descricao;
     private int impactoXP;
+    private int impactoCansaco;
+    private int impactoDias;
 
-    public Desafio(String descricao, int impactoXP) {
+    public Desafio(String descricao, int impactoXP, int impactoCansaco, int impactoDias) {
         this.descricao = descricao;
         this.impactoXP = impactoXP;
+        this.impactoCansaco = impactoCansaco;
+        this.impactoDias = impactoDias;
     }
 
     public void aplicarConsequencia(Desenvolvedor dev) {
+        System.out.println("Um desafio ocorreu: " + descricao);
+        
         dev.adicionarXP(-impactoXP);
-        System.out.println("⚠️ Desafio enfrentado: " + descricao + " | -" + impactoXP + " XP");
+        dev.forcarAumentoCansaco(impactoCansaco);
+        dev.forcarAumentoDias(impactoDias);
+
+        System.out.println("Perdeu " + impactoXP + " XP.");
+        System.out.println("Ganhou " + impactoCansaco + " de cansaço.");
+        System.out.println("Dias avançaram " + impactoDias + ".");
     }
 }
